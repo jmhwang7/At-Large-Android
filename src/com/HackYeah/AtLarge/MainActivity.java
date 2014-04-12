31d320bf126e557e2245cb01256029496f8644a6
jmhwang7 <jmhwang7@gmail.com>
2014-04-11 20:42:51 -0400

    Start to FB Login

--- src/com/HackYeah/AtLarge/MainActivity.java
+++ src/com/HackYeah/AtLarge/MainActivity.java
@@ -1,15 +1,23 @@
 package com.HackYeah.AtLarge;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -17,11 +25,36 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		
+		// start Facebook Login
+		Session.openActiveSession(this, true, new Session.StatusCallback() {
+			
+			// callback when session changes state
+			@SuppressWarnings("deprecation")
+			@Override
+			public void call(Session session, SessionState state, Exception exception) {
+				if (session.isOpened()) {
+					Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
+						
+						// callback after Graph API response with user object
+						@Override
+						public void onCompleted(GraphUser user, Response response) {
+							Log.e("MAIN", "got user object");
+							if (user != null) {
+								TextView welcome = (TextView) findViewById(R.id.welcome);
+								welcome.setText("Hello " + user.getName() + "!");
+							}
+							
+						}
+					});
+				}
+				
+			}
+		});
+//		if (savedInstanceState == null) {
+//			getSupportFragmentManager().beginTransaction()
+//					.add(R.id.container, new PlaceholderFragment()).commit();
+//		}
 	}
 
 	@Override
@@ -59,6 +92,12 @@ public class MainActivity extends ActionBarActivity {
 					false);
 			return rootView;
 		}
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
 	}
 
 }
